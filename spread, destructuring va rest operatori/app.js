329ffa1c45aaf6eb7f4ac55c6bdd47328855128e
Jahongir <bahromovam843@gmail.com>
2024-07-13 09:57:52 -0800

    2-dars
    destructuring - yordamida biz arraydagi qiymatlarni va obyektdagi xossalarni ajratib olishimiz mumkin.

---
+++ spread, destructuring va rest operatori/app.js
@@ -0,0 +1,30 @@
+// 1
+const nums = [10, 15, 20, 25];
+
+const [firstvalue, ...othervalues] = nums;
+console.log(firstvalue);
+console.log(othervalues);
+
+// 2
+const user = {
+    name: 'Jonpulat',
+    lastname: 'Ravshanov',
+    age: 16,
+}
+
+const {name, lastname} = user;
+console.log(name, lastname);
+
+
+// 3
+const total = (...numbers) => {
+    return numbers.reduce((num, num2) => num + num2, 0);
+}
+
+console.log(total(10, 19, 24, 40));
+
+// 4
+const person = {name: 'Jahongir', age: 16 };
+const userWithTraining = { ...person, training: 'playing football'};
+
+console.log(userWithTraining);
